d082a8ec796d38050877c001855697cc1ad7462b
Alan L <alan.leung57@gmail.com>
2018-09-18 12:48:15 -0600

    Added code so that all fields can be saved and loaded upon page refresh

--- app.js
+++ app.js
@@ -7,15 +7,20 @@
 // Future:
 // - Warning if number registered exceeds capacity
 // 
+// Trying to use doc comments as per https://github.com/Microsoft/tsdoc 
+// 
 // Acknowledgements:
 // - https://www.sitepoint.com/fetching-data-third-party-api-vue-axios/
 // - 
 // - https://vuejsdevelopers.com/2017/05/20/vue-js-safely-jquery-plugin/
 //
 
+// Vue component wrapping JQuery's datepicker.
+// See https://vuejsdevelopers.com/2017/05/20/vue-js-safely-jquery-plugin/
+//
 Vue.component('date-picker', {
-  template: '<input v-bind:placeholder="placeholderValue" />',
-  props: [ 'dateFormat', 'placeholderValue' ],
+  template: '<input v-bind:placeholder="placeholderValue" v-bind:value="initialValue" />',
+  props: [ 'dateFormat', 'placeholderValue', 'initialValue' ],
   mounted: function() {
 
     var self = this;
@@ -82,16 +87,21 @@ const vm = new Vue({
         console.log("Finished processing data at " + new Date());        
 
         // Load previous selections 
-        if (localStorage.getItem('selectedFacilities')) this.selectedFacilities = JSON.parse(localStorage.getItem('selectedFacilities'));
-        if (localStorage.getItem('selectedStartDate')) this.selectedStartDate = JSON.parse(localStorage.getItem('selectedStartDate'));
-        if (localStorage.getItem('includeFutureActivities')) this.includeFutureActivities = JSON.parse(localStorage.getItem('includeFutureActivities'));
-        
+        this.selectedFacilities = this.loadPreviousSelection('selectedFacilities', this.selectedFacilities);
+        this.selectedStartDate = this.loadPreviousSelection('selectedStartDate', this.selectedStartDate);
+        this.includeFutureActivities = this.loadPreviousSelection('includeFutureActivities', this.includeFutureActivities);
+        this.selectedAgeRanges = this.loadPreviousSelection('selectedAgeRanges', this.selectedAgeRanges);
+        this.selectedActivityTypes = this.loadPreviousSelection('selectedActivityTypes', this.selectedActivityTypes);
+      }).catch(function (error) {
+
+        // e.g. Sometimes deserialization may not work because of problems during serialization to localStorage
+        console.log(error);
       })
   }, 
   methods: {
+    // @param activityDetails - see sample-data.json for object attributes 
+    // @returns true or false 
     includeResult(activityDetails) {
-      // activityDetails - see sample-data.json for object attributes 
-      // Returns true or false 
 
       var doInclude = true;
       if (doInclude) {
@@ -107,27 +117,48 @@ const vm = new Vue({
       return doInclude;
     },
     updateStartDate(date) {
+
       this.selectedStartDate = date;
+    },
+    loadPreviousSelection(key, defaultValue) {
+
+      if (localStorage.getItem(key)) {
+        return JSON.parse(localStorage.getItem(key));
+      } else {
+        // Empty string seems to work better than undefined 
+        return defaultValue;
+      }
+    },
+    saveSelection(key, value) {
+
+      localStorage.setItem(key, JSON.stringify(value));
     }
   },
   watch: {
     selectedFacilities: {
       handler() { 
-        localStorage.setItem('selectedFacilities', JSON.stringify(this.selectedFacilities));
+        this.saveSelection('selectedFacilities', this.selectedFacilities);
       }
     },
     selectedStartDate: {
       handler() { 
-        localStorage.setItem('selectedStartDate', JSON.stringify(this.selectedStartDate));
+        this.saveSelection('selectedStartDate', this.selectedStartDate);
       }
     },  
     includeFutureActivities: {
       handler() { 
-        localStorage.setItem('includeFutureActivities', JSON.stringify(this.includeFutureActivities));
+        this.saveSelection('includeFutureActivities', this.includeFutureActivities);
       }
     },
-    // selectedAgeRanges: [],
-    // selectedActivityTypes: [],
-
+    selectedAgeRanges: {
+      handler() { 
+        this.saveSelection('selectedAgeRanges', this.selectedAgeRanges);
+      }
+    },
+    selectedActivityTypes: {
+      handler() { 
+        this.saveSelection('selectedActivityTypes', this.selectedActivityTypes);
+      }
+    }
   }
 });
